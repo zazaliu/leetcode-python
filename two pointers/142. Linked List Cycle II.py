7f632c212ec93f7ec9ebfcb7149585a272e179f5
刘自豪 <1004657064@qq.com>
2017-12-11 15:17:43 +0800

    # Solution for 19,141,142

---
+++ two pointers/142. Linked List Cycle II.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        fast,slow=head,head
+        while fast and fast.next:
+            fast,slow=fast.next.next,slow.next
+            if fast == slow:
+                break
+        if fast == None or fast.next == None:
+            return None
+        slow=head
+        while fast != slow:
+            fast,slow=fast.next,slow.next
+        return fast
